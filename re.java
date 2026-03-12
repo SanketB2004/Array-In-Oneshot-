2aab51b92730a92e1f6e723da079c119be04fc9e
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-03-12 12:05:30 +0000

    Okk

--- re.java
+++ re.java
@@ -6,9 +6,23 @@ public class re {
     public static void main(String[] args) {
       int [] arr = {1,2,3,4,5};
 
-int start = 0;
+// int start = 1;
 
-int end = arr.length-1;
+// int end = arr.length-3;
+
+// while (start < end) {
+    
+//      int temp = arr[start];
+     
+//      arr[start] = arr[end];
+//      arr[end] = temp;
+
+//      start++;
+//      end--;
+
+int start = 1;
+
+int end = 3;
 
 while (start < end) {
     
