2200ce672cd682c3172acc739eb412bb9595a0e5
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-03-12 07:58:15 +0000

    ok

---
+++ twoSum.java
@@ -0,0 +1,21 @@
+
+public class TwoSum {
+    public static void main(String[] args) {
+        
+        int[] arr1 = {1,88,1,4,5,6,7,8,9,10};
+        int target = 2;
+
+        for (int i = 0; i < arr1.length; i++) {
+
+            for (int j = i + 1; j < arr1.length; j++) {
+
+                if (arr1[i] + arr1[j] == target) {
+                    System.out.println("True");
+                }
+
+            }
+        }
+    }
+}
+                
+         
