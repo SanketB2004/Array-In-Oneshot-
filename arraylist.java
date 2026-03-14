d840849e7d72c83def8eec6a70a1a78157effe36
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-03-14 08:18:21 +0000

    okk

--- arraylist.java
+++ arraylist.java
@@ -14,30 +14,47 @@ public class arraylist {
         arr.add(650);
         arr.add(750);
     
-        System.out.println(arr.get(5));
+    //     System.out.println(arr.get(5));
 
-        arr.set(5, 5000);
+    //     arr.set(5, 5000);
 
-        System.out.println(arr.get(5));
+    //     System.out.println(arr.get(5));
 
-        System.out.println(arr);
+    //     System.out.println(arr);
 
-        int n = arr.size();
+    //     int n = arr.size();
 
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr.get(i)+ " ");
+    //     for (int i = 0; i < n; i++) {
+    //         System.out.print(arr.get(i)+ " ");
 
-        }
-    for (int ele : arr) {
-        System.out.print(  "\n"+ ele + " ");
-    }
+    //     }
+    // for (int ele : arr) {
+    //     System.out.print(  "\n"+ ele + " ");
+    // }
 
-    arr.add( 45, 4555555);
+    // arr.add( 45, 4555555);
 
    
-    
+    // array element delet 
 
-    
+    //arr.remove(arr.size()-1);
 
-    }
+    System.out.println(arr);
+
+    //Collections.reverse(arr);  // Reverse Method Of Array 
+
+    System.out.println(arr);
+int i = 0 ,  j = arr.size()-1;
+
+ while (i<j) {
+    int temp = arr.get(i);
+arr.set(i, arr.get(j));
+
+arr.set(j,temp);
+
+i++;
+j--;
 }
+System.out.println(arr);
+    }
+}// reverse arraylist
